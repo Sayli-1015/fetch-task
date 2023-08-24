7ac10fcf236962235289210e32134279cbe7e91e
Sayli-1015 <sayli.t@sankeysolutions.com>
2023-08-24 14:50:13 +0530

    feat: added table

--- index.js
+++ index.js
@@ -34,14 +34,17 @@ const api_url = 'https://run.mocky.io/v3/9b184f9d-bf48-4467-9d8f-137ea0eba817';
                 </tr>
             `;
 
-            const rows = data.map(r => `
-                <tr>
+            var rows = '';
+            for (var i = 0; i < data.length; i++) {
+                var r = data[i];
+                var row = `<tr> 
                     <td>${r.name}</td>
                     <td>${r.office}</td>
                     <td>${r.position}</td>
                     <td>${r.salary}</td>
-                </tr>
-            `).join('');
+                </tr>`;
+                rows += row;
+    }
 
             employeesTable.innerHTML = headerRow + rows;
         }
