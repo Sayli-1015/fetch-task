5fb6618dc7b6807afe9901b4f47c99107fd33db9
Sayli-1015 <sayli.t@sankeysolutions.com>
2023-08-24 12:42:38 +0530

    feat: added fetch datat

---
+++ index.js
@@ -0,0 +1,49 @@
+const api_url = 'https://run.mocky.io/v3/9b184f9d-bf48-4467-9d8f-137ea0eba817';
+        const loadingElement = document.getElementById('loading');
+        const employeesTable = document.getElementById('employees');
+
+        async function fetchAPI(url) {
+            try {
+                const response = await fetch(url);
+                const data = await response.json();
+                console.log(data);
+
+                if (response.ok) {
+                    hideLoader();
+                    showData(data.data);
+                } else {
+                    console.error('API request failed:', response.statusText);
+                }
+            } catch (error) {
+                loadingElement.innerHTML = 'Error fetching data';
+                console.error('Error fetching data:', error);
+            }
+        }
+
+        function hideLoader() {
+            loadingElement.style.display = 'none';
+        }
+
+        function showData(data) {
+            const headerRow = `
+                <tr>
+                    <th>Name</th>
+                    <th>Office</th>
+                    <th>Position</th>
+                    <th>Salary</th>
+                </tr>
+            `;
+
+            const rows = data.map(r => `
+                <tr>
+                    <td>${r.name}</td>
+                    <td>${r.office}</td>
+                    <td>${r.position}</td>
+                    <td>${r.salary}</td>
+                </tr>
+            `).join('');
+
+            employeesTable.innerHTML = headerRow + rows;
+        }
+fetchAPI(api_url);
+    
